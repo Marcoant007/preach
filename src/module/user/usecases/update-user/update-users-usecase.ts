0f311bcd30b95f45ae7e0f326802694652bd9242
Marcoant007 <marcoantnovo@gmail.com>
2023-06-27 23:24:23 -0300

    Adding swagger and adjusting the application's use case update business rule

--- src/module/user/usecases/update-user/update-users-usecase.ts
+++ src/module/user/usecases/update-user/update-users-usecase.ts
@@ -16,8 +16,12 @@ class UpdateUserUseCase {
     async execute(userId: number, userData: UserDTO): Promise<UserDTO>{
         const passwordHash = await hash(userData.password, 8);
         const userExist = await this.userRepository.findUserById(userId);
+        const emailExist = await this.userRepository.findUserByEmail(userData.email);
         if(!userExist){
-            throw new AppError("User not exists");
+            throw new AppError("User Not Exists");
+        }
+        if(emailExist){
+            throw new AppError("Email Already Exists")
         }
         userData.password = passwordHash;
         const userUpdated = await this.userRepository.updateUsers(userId, userData);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { authenticateRoutes } from "./login.routes";
 import usersRoutes from "./users.routes";
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../utils/swagger.json';
 
 const router = Router();
 
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 router.use('/users', usersRoutes);
 router.use(authenticateRoutes);
 
